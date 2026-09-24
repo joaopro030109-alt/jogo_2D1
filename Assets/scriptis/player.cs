270c6c57bb713a8255a3bd582f0092855f1f19e8
pedrolucaslucindo-007 <joaopro030109@gmail.com>
2026-09-24 09:34:41 -0300

    mapa
    hoje nos criamos o mapa do jogo com nossas temas da programação

--- Assets/scriptis/player.cs
+++ Assets/scriptis/player.cs
@@ -22,14 +22,14 @@ public class player : MonoBehaviour
         rb.linearVelocity = new Vector2(moveHorizontal * speed, rb.linearVelocity.y); 
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
-            rb.AddForce(new Vector2(0F, 5F), ForceMode2D.Impulse);//vai reconhecer quando o player apertar espaço para asionar o pulo 
+            rb.AddForce(new Vector2(0F, 6F), ForceMode2D.Impulse);//vai reconhecer quando o player apertar espaço para asionar o pulo 
         }
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Graund"))
         {
-            isGrounded = true; //vai reconhecer quando o jogador encostar em um objeto que temcolisor 
+            isGrounded = true; //vai reconhecer quando o jogador encostar em um objeto que tem colisor 
 
         }
     }
